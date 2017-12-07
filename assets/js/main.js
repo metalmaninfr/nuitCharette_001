3bc1eb63ecd0f80fb8a1f50e72f458a5b1119900
mallano <maelallano1@gmail.com>
2017-12-07 23:06:01 +0100

    ready to go !

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// <!-- <html>
+//     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+//     <button id="click2">Click me to go to Test 2</button>
+    
+//     <script>
+//         $(document).ready(function (){
+//             $("#click").click(function (){
+//                 $('html, body').animate({
+//                     scrollTop: $("#div1").offset().top
+//                 }, 1000);
+//             });
+//         });
+//     </script>
+//     <script>
+//         $(document).ready(function (){
+//             $("#click2").click(function (){
+//                 $('html, body').animate({
+//                     scrollTop: $("#div2").offset().top
+//                 }, 1000);
+//             });
+//         });
+//     </script>
+//     <div id="div1" style="height: 1000px; width 100px">
+//         Test
+//     </div>
+//     <br/>
+//     <div id="div2" style="height: 1000px; width 100px">
+//         Test 2
+//     </div>
+//     <button id="click">Click me to go to Test</button>
+// </html> -->
+
+console.log('wesh')
